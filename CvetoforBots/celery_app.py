05db06c41f16b2bfd788214eafc9236cb8e6d674
S3raphimCS <700520serj@mail.ru>
2025-07-10 22:38:17 +1000

    [feature] Добавил свойство celery на отмену при потере соединения

--- CvetoforBots/celery_app.py
+++ CvetoforBots/celery_app.py
@@ -9,3 +9,4 @@ app = Celery('server')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 app.conf.timezone = 'UTC'
+app.conf.update(worker_cancel_long_running_tasks_on_connection_loss=True)
